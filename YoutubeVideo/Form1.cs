b50106f687146727fa44553b7c939b9388e8ad5b
TheRealJMV <jeanmarcve@hotmail.fr>
2018-12-08 10:56:41 +0100

    All new

--- YoutubeVideo/Form1.cs
+++ YoutubeVideo/Form1.cs
@@ -5,16 +5,180 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Media;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32; //Registry...
+using System.Diagnostics; // Process.GetCurrentProcess...
+
 
 namespace YoutubeVideo
 {
     public partial class Form1 : Form
     {
+        private void SetBrowserFeatureControlKey(string feature, string appName, uint value)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(
+                String.Concat(@"Software\Microsoft\Internet Explorer\Main\FeatureControl\", feature),
+                RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.SetValue(appName, (UInt32)value, RegistryValueKind.DWord);
+            }
+        }
+
+        private void SetBrowserFeatureControl()
+        {
+            // http://msdn.microsoft.com/en-us/library/ee330720(v=vs.85).aspx
+
+            // FeatureControl settings are per-process
+            var fileName = System.IO.Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+
+            // make the control is not running inside Visual Studio Designer
+            if (String.Compare(fileName, "devenv.exe", true) == 0 || String.Compare(fileName, "XDesProc.exe", true) == 0)
+                return;
+
+            SetBrowserFeatureControlKey("FEATURE_BROWSER_EMULATION", fileName, GetBrowserEmulationMode()); // Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode.
+            SetBrowserFeatureControlKey("FEATURE_AJAX_CONNECTIONEVENTS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_MANAGE_SCRIPT_CIRCULAR_REFS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_DOMSTORAGE ", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_GPU_RENDERING ", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_IVIEWOBJECTDRAW_DMLT9_WITH_GDI  ", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_DISABLE_LEGACY_COMPRESSION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_LOCALMACHINE_LOCKDOWN", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_OBJECT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_SCRIPT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_DISABLE_NAVIGATION_SOUNDS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_SPELLCHECKING", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_STATUS_BAR_THROTTLING", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_TABBED_BROWSING", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_VALIDATE_NAVIGATE_URL", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_DOCUMENT_ZOOM", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_POPUPMANAGEMENT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_MOVESIZECHILD", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_ADDON_MANAGEMENT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_WEBSOCKET", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WINDOW_RESTRICTIONS ", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_XMLHTTP", fileName, 1);
+        }
+
+        private UInt32 GetBrowserEmulationMode()
+        {
+            int browserVersion = 7;
+            using (var ieKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer",
+                RegistryKeyPermissionCheck.ReadSubTree,
+                System.Security.AccessControl.RegistryRights.QueryValues))
+            {
+                var version = ieKey.GetValue("svcVersion");
+                if (null == version)
+                {
+                    version = ieKey.GetValue("Version");
+                    if (null == version)
+                        throw new ApplicationException("Microsoft Internet Explorer is required!");
+                }
+                int.TryParse(version.ToString().Split('.')[0], out browserVersion);
+            }
+
+            UInt32 mode = 11000; // Internet Explorer 11. Webpages containing standards-based !DOCTYPE directives are displayed in IE11 Standards mode. Default value for Internet Explorer 11.
+            switch (browserVersion)
+            {
+                case 7:
+                    mode = 7000; // Webpages containing standards-based !DOCTYPE directives are displayed in IE7 Standards mode. Default value for applications hosting the WebBrowser Control.
+                    break;
+                case 8:
+                    mode = 8000; // Webpages containing standards-based !DOCTYPE directives are displayed in IE8 mode. Default value for Internet Explorer 8
+                    break;
+                case 9:
+                    mode = 9000; // Internet Explorer 9. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
+                    break;
+                case 10:
+                    mode = 10000; // Internet Explorer 10. Webpages containing standards-based !DOCTYPE directives are displayed in IE10 mode. Default value for Internet Explorer 10.
+                    break;
+                default:
+                    // use IE11 mode by default
+                    break;
+            }
+
+            return mode;
+        }
         public Form1()
         {
+            SetBrowserFeatureControl();
+            
             InitializeComponent();
+            //SetWebBrowserFeatures(GetBrowserVersion());
+
+            this.webBrowser1.Navigate("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            //youtubePlayer.Navigate(new Uri("https://developers.google.com/youtube/youtube_player_demo"));
+            //axShockwaveFlash1.Movie = "https://www.youtube.com/v/v2fDTOdWuQQ?rel=0&amp;showinfo=0";
+            //this.ClientSize = new System.Drawing.Size(880, 510);
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Dock = DockStyle.Fill;
+            this.Controls.Add(this.webBrowser1);
         }
+
+        /// <summary>
+        /// Modify the registry information to be compatible with the current program
+        /// 
+        /// </summary>
+        static void SetWebBrowserFeatures(int ieVersion)
+        {
+            if (LicenseManager.UsageMode != LicenseUsageMode.Runtime)
+                return;
+            var appName = System.IO.Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            UInt32 ieMode = GeoEmulationModee(ieVersion);
+            var featureControlRegKey = @"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\";
+            Registry.SetValue(featureControlRegKey + "FEATURE_BROWSER_EMULATION",
+                appName, ieMode, RegistryValueKind.DWord);
+            Registry.SetValue(featureControlRegKey + "FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION",
+                appName, 1, RegistryValueKind.DWord);
+        }
+
+        static int GetBrowserVersion()
+        {
+            int browserVersion = 0;
+            using (var ieKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer",
+                RegistryKeyPermissionCheck.ReadSubTree,
+                System.Security.AccessControl.RegistryRights.QueryValues))
+            {
+                var version = ieKey.GetValue("svcVersion");
+                if (null == version)
+                {
+                    version = ieKey.GetValue("Version");
+                    if (null == version)
+                        throw new ApplicationException("Microsoft Internet Explorer is required!");
+                }
+                int.TryParse(version.ToString().Split('.')[0], out browserVersion);
+            }
+            if (browserVersion < 7)
+            {
+                throw new ApplicationException("NO SUPPORT!");
+            }
+            return browserVersion;
+        }
+
+
+        static UInt32 GeoEmulationModee(int browserVersion)
+        {
+            UInt32 mode = 11000; // Internet Explorer 11. Webpages containing standards-based !DOCTYPE directives are displayed in IE11 Standards mode. 
+            switch (browserVersion)
+            {
+                case 9:
+                    mode = 9000; // Internet Explorer 9. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode.                    
+                    break;
+                case 10:
+                    mode = 10000; // Internet Explorer 10.
+                    break;
+                case 11:
+                    mode = 11000; // Internet Explorer 11
+                    break;
+            }
+            return mode;
+        }
+
+
     }
+
 }
